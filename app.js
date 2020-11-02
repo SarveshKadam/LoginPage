0fe29ac64c3edc0d74818d644ea009cb3013c627
unknown <kadamsarvesh1905@gmail.com>
2020-11-02 20:57:30 +0530

    Add dynamic property

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const port = process.env.PORT || 3000
 
 app.set('view engine','ejs')
 app.use(express.static(__dirname+ '/public'))
+app.use(helmet())
 
 app.get('/',(req,res)=>{
     res.render('login')
 })
 
-app.post('/landing',(req,res)=>{
-    res.render('landing')
+app.post('/landing',urlencodedParser,(req,res)=>{
+    const username = req.body.username
+    res.render('landing',{username})
 })
 
 app.listen(port,()=>{
