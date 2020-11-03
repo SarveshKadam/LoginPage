5679f5a20546d75d0b18274f15f4888351d1d5dc
unknown <kadamsarvesh1905@gmail.com>
2020-11-03 16:14:08 +0530

    cookie data

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const helmet = require('helmet')
 const app = express()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const port = process.env.PORT || 3000
 
 app.set('view engine','ejs')
 app.use(express.static(__dirname+ '/public'))
+app.use(express.urlencoded())
 app.use(helmet())
+app.use(cookieParser())
 
-app.get('/',(req,res)=>{
+
+app.use((req,res,next)=>{
+    if ( req.query.msg === 'fail') {
+        res.locals.msg = "Sorry, this username and password combination does not exists"
+    } else {
+        res.locals.msg= ''
+    }
+   next()
+})
+app.get('/login',(req,res)=>{
     res.render('login')
 })
 
-app.post('/landing',urlencodedParser,(req,res)=>{
+app.post('/loginprocess',(req,res)=>{
     const username = req.body.username
-    res.render('landing',{username})
+    const password = req.body.password
+    if(password === 'test'){
+    res.cookie('username',username)
+    res.redirect('/landing')
+    }else{
+        res.redirect('login?msg=fail')
+    }
+})
+
+app.get('/landing',(req,res)=>{
+    res.render('landing',{username : req.cookies.username})
+})
+
+app.get('/logout',(req,res)=>{
+    res.clearCookie('username')
+    res.redirect('login')
 })
 
 app.listen(port,()=>{
